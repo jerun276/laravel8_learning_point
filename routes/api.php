3bf5d329104bafa96f9a59cd27b73fc62d1c3384
Jerun Kingston <jerunkingston@gmail.com>
2023-09-26 09:42:57 +0530

    routes studied

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/hello',function($name=null){
+    return 'hello world ';
+});
+
+Route::get('/users/{id?}/{name?}',function($id=null,$name="Laravel"){
+    return 'My id is : '.$id.' my name is '.$name;
+});
+
+Route::get('/regex/{name?}',function($name="Laravel"){
+    return 'My name is '.$name;
+})->where('name','[A-Za-z]+');
+
+Route::match(['get','post'],'/matchtest', function (Request $req) {
+    return "My method is : ".$req->method();
+});
+
+Route::any('/matchtestany', function (Request $req) {
+    return "My request method is : ".$req->method();
+});
+
